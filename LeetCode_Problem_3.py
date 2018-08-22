cb45334a0f09a4b1cf00ca2ff933c55a22f5af56
Amit Bishnoi <bishnoiamit884@gmail.com>
2018-08-22 12:59:38 -0700

    Added Problem 3 Solution
    The solution is implemented in Python3.

---
+++ LeetCode_Problem_3.py
@@ -0,0 +1,42 @@
+
+"""
+Created on Wed Aug 22 12:48:41 2018
+
+@author: bishnoiamit
+"""
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """  
+        tlist = []
+        f_count = 0    
+        
+        olist = list(s)
+        
+        while(olist):
+            elem = olist.pop(0)                
+            
+            if elem in tlist:
+                count = len(tlist)
+                if f_count < count:
+                    f_count = count
+                del tlist[:(tlist.index(elem))+1]
+            tlist.append(elem)
+
+            
+        count = len(tlist)
+        if f_count < count:
+            f_count = count   
+        return f_count
+
+def main():
+    obj = Solution()
+    s = input("Please enter the string:")
+    final_length = obj.lengthOfLongestSubstring(s)    
+    print("Length of the longest substring is: %d" %final_length)
+
+
+main()
